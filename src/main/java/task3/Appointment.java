53af1cc7ce322c78717d4383de4cec444e445cec
malinabooy <1anejlbcuh4uk1@gmail.com>
2023-11-19 15:00:18 +0300

    seminar3 - initial commit

---
+++ src/main/java/task3/Appointment.java
@@ -0,0 +1,26 @@
+package task3;
+
+class Appointment {
+    private Doctor doctor;
+    private Patient patient;
+    private Date date;
+
+    public Appointment(Doctor doctor, Patient patient, Date date) {
+        this.doctor = doctor;
+        this.patient = patient;
+        this.date = date;
+    }
+
+    public Doctor getDoctor() {
+        return 0;
+    }
+
+    public Patient getPatient() {
+        return 0;
+    }
+
+    public Date getDate() {
+        return 0;
+    }
+}
+
---
+++ src/test/java/task3/AppointmentTest.java
@@ -0,0 +1,25 @@
+package task3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getDoctor() {
+    }
+
+    @Test
+    void getPatient() {
+    }
+
+    @Test
+    void getDate() {
+    }
+}
